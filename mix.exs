24791ae7799ae91685fd147a39fee091bf0861f6
Vinicius Batista <viniciusbfs2012@gmail.com>
2018-10-03 15:03:53 -0300

    add credo linter

--- mix.exs
+++ mix.exs
@@ -38,7 +38,8 @@ defmodule Easypub.Mixfile do
       {:phoenix_ecto, "~> 3.2"},
       {:postgrex, ">= 0.0.0"},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:credo, "~> 0.10", only: [:dev, :test], runtime: false}
     ]
   end
 
