0fb157685f029d6059d23bcf2760e5ac4c56a928
Vinicius Batista <viniciusbfs2012@gmail.com>
2018-10-15 19:31:54 -0300

    fix syntax error in rel/config.ex

--- rel/config.exs
+++ rel/config.exs
@@ -7,14 +7,13 @@ Path.join(["rel", "plugins", "*.exs"])
 |> Enum.map(&Code.eval_file(&1))
 
 use Mix.Releases.Config,
-    # This sets the default release built by `mix release`
-    default_release: :default,
-    # This sets the default environment used by `mix release`
-    default_environment: Mix.env()
+  # This sets the default release built by `mix release`
+  default_release: :default,
+  # This sets the default environment used by `mix release`
+  default_environment: Mix.env()
 
 # For a full list of config options for both releases
 # and environments, visit https://hexdocs.pm/distillery/configuration.html
-
 
 # You may define one or more environments in this file,
 # an environment's settings will override those of a release
@@ -28,15 +27,15 @@ environment :dev do
   # It is recommended that you build with MIX_ENV=prod and pass
   # the --env flag to Distillery explicitly if you want to use
   # dev mode.
-  set dev_mode: true
-  set include_erts: false
-  set cookie: :"q,pXe}fep!{@LMS3dn3w*lxQbX=[$`J&5=ENRuCExmWjt]sopHt[HuP=n{:>Hqzi"
+  set(dev_mode: true)
+  set(include_erts: false)
+  set(cookie: :"q,pXe}fep!{@LMS3dn3w*lxQbX=[$`J&5=ENRuCExmWjt]sopHt[HuP=n{:>Hqzi")
 end
 
 environment :prod do
-  set include_erts: true
-  set include_src: false
-  set cookie: :"8@C_vr8YfM$ZMgxbqQoqC$zra=r6ba5dzDJ>pm!Ho[nwOuIEqB[M7v]:xojyzrO?"
+  set(include_erts: true)
+  set(include_src: false)
+  set(cookie: :"8@C_vr8YfM$ZMgxbqQoqC$zra=r6ba5dzDJ>pm!Ho[nwOuIEqB[M7v]:xojyzrO?")
 end
 
 # You may define one or more releases in this file.
@@ -45,13 +44,18 @@ end
 # will be used by default
 
 release :easypub do
-  set version: current_version(:easypub)
-  set applications: [
-    :runtime_tools
-  ],
-  set commands: [
-    migrate: "rel/commands/migrate.sh",
-    seed: "rel/commands/seed.sh",
-  ]
-end
+  set(version: current_version(:easypub))
 
+  set(
+    applications: [
+      :runtime_tools
+    ]
+  )
+
+  set(
+    commands: [
+      migrate: "rel/commands/migrate.sh",
+      seed: "rel/commands/seed.sh"
+    ]
+  )
+end
