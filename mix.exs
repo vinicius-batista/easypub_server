400a2e2b68551843e61897e88d14e108d348a793
Vinicius Batista <viniciusbfs2012@gmail.com>
2018-10-21 17:32:25 -0300

    bump 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Easypub.Mixfile do
   def project do
     [
       app: :easypub,
-      version: "0.1.11",
+      version: "0.2.0",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
